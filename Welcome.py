cce4b83aaf40c629a3cb717709fcf864533cf51f
Kevin <101021025+kevdmorris@users.noreply.github.com>
2022-05-09 23:57:25 -0400

    Add files via upload

---
+++ Welcome.py
@@ -0,0 +1,16 @@
+# Allow user to input their First Name and assign that input to a variable
+
+
+# Allow user to input their Last Name and assign that input to a variable
+
+
+# Print a welcome message that includes the user's First Name and Last Name using the variables.
+
+
+
+fname = input ("Enter first name:") #First name variable
+
+lname = input ("Enter last name:") #Last name variable
+
+print ("Welcome to your very 1st python script " + fname, lname )
+
