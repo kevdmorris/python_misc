960f709f2a681e66c0fae1ded3c3221e5723e976
Kevin <101021025+kevdmorris@users.noreply.github.com>
2022-05-10 23:44:48 -0400

    Add files via upload

---
+++ new 5.py
@@ -0,0 +1,16 @@
+services = []
+ 
+ # Add users to the list
+ 
+services.append('S3')
+services.append('Lambda')
+services.append('EC2')
+services.append('Cloud Trail')
+services.append('Cloud Watch')
+services.append('AWS X-Ray')
+services.append('Dynamo DB')
+services.append('Amazon RDS')
+services.append('Code Deploy')
+services.append('Code Commit')
+
+print services
