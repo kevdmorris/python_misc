ab091197ce22f3e5efb317ea97f294188bc63e65
EC2 Default User <ec2-user@ip-172-31-1-51.ec2.internal>
2022-05-19 00:53:44 +0000

    updated new branch

--- number_game.py
+++ number_game.py
@@ -3,12 +3,22 @@ Guess the number game
 """
 import random
 
-value = str(input("Please enter a number from 0 - 100: "))
+value = int(input("Please enter a number from 0 - 100: "))
 
 number = random.randint(0,100)
 
+print(number)
+
+if number == 13:
+
+    print("Your are correct!")
+
 if number > 13:
 
-    print(" Your are too high")
+    print("Your are too high.")
+
+if number < 13:
+
+    print("You are too low.")
 
 #print (number, " < 13: Your number is too low")
