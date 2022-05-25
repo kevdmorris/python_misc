c515d710f6478e8758fcf563a6d26c8fac94e98f
EC2 Default User <ec2-user@ip-172-31-5-102.ec2.internal>
2022-05-25 10:15:56 +0000

    Updated ImageId

--- ec2createmulti.py
+++ ec2createmulti.py
@@ -2,7 +2,7 @@
 
 import boto3
 ec2_resource=boto3.resource("ec2")
-ec2_resource.create_instances(ImageId='ami-03657b56516ab7912',
+ec2_resource.create_instances(ImageId='ami-0022f774911c1d690',
       InstanceType='t2.micro',
     MaxCount=4,
       MinCount=3)
