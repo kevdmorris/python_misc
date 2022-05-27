4de4b55eb266a041637e00436e70a37d01fc4a29
EC2 Default User <ec2-user@ip-172-31-5-102.ec2.internal>
2022-05-27 02:40:02 +0000

    MOre AWS lab

---
+++ simple_loop.py
@@ -0,0 +1,21 @@
+#simple loop with strings
+
+#fruit = ['apples','oranges','bananas']
+
+#for item in fruit:
+ #   print(f'The best fruit now is {item}')
+
+##########################################################
+#Simple loop with numbers
+
+numbers = [0,1,2,3,4,5,6,7,8,9,10]
+#for number in numbers:
+# print(f'The next number is {number}')
+
+##################################################################
+#Range Function
+#for number in range(10):
+#for number in range(1,10):
+for number in range(1,10,2):
+  print(f'The next number is {number}')
+
