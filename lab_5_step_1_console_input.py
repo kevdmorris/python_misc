4e6795efbc682bddaf3c8b14ddfd645b4d145e01
EC2 Default User <ec2-user@ip-172-31-5-102.ec2.internal>
2022-05-27 02:21:20 +0000

    AWS Lab 4 & 5

---
+++ lab_5_step_1_console_input.py
@@ -0,0 +1,35 @@
+import boto3
+
+def translate_text(**kwargs): 
+    client = boto3.client('translate')
+    response = client.translate_text(**kwargs)
+    print(response) 
+
+### Change below this line only ###
+
+#kwargs={
+#    "Text":"I am learning to code in AWS",
+#    "SourceLanguageCode":"en",
+ #   "TargetLanguageCode":"fr"
+ #   }
+
+#def main():
+#    translate_text(**kwargs)
+
+#if __name__=="__main__":
+#    main()
+
+text = input("Provide the text you want translating: ")
+source_language_code = input("Provide the two letter source language code: ")
+target_language_code = input("Provide the two letter target language code: ") 
+
+def main():
+    translate_text(
+        Text=text,
+        SourceLanguageCode=source_language_code,
+        TargetLanguageCode=target_language_code
+        )
+
+if __name__=="__main__":
+    main()
+
