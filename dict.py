0d61d6dcb22f947668783021d59bb61887559a2c
EC2 Default User <ec2-user@ip-172-31-5-102.ec2.internal>
2022-05-27 01:40:08 +0000

    AWS Labs

---
+++ dict.py
@@ -0,0 +1,23 @@
+Return type
+    dict
+
+Returns
+    Response Syntax
+
+    {
+        'TranslatedText': 'string',
+        'SourceLanguageCode': 'string',
+        'TargetLanguageCode': 'string',
+        'AppliedTerminologies': [
+            {
+                'Name': 'string',
+                'Terms': [
+                    {
+                        'SourceText': 'string',
+                        'TargetText': 'string'
+                    },
+                ]
+            },
+        ]
+    }
+
